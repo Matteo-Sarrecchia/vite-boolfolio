d987a2fcfa7fcfed79e51af199cf1403901537e9
Matteo Sarrecchia <morrea@hotmail.it>
2023-08-22 10:53:37 +0200

    components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,7 +36,7 @@ export default {
   <div class="container">
     <h1>Projects</h1>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class=" card" v-for="project in projects" :key="project.id">
+      <div class="card py-3" v-for="project in projects" :key="project.id">
         [{{ project.id }}] {{ project.title }}
 
       </div>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'; // Importa il file CSS di Bootstrap
+import 'bootstrap'; // Importa gli script JavaScript di Bootstrap
 createApp(App).mount('#app')
