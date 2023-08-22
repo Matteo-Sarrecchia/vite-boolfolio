b575e6b50a52f4bf1569805fff13516f3e12c692
Matteo Sarrecchia <morrea@hotmail.it>
2023-08-22 10:16:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container-fluid  bg-primary text-center text-white">
+    <HelloWorld />
+
+  </div>
+</template>
